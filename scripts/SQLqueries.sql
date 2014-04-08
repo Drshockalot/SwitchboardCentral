e5def69fa356f03463c3c38c737402268d0b1a51
Lukeeward <b2029843@my.shu.ac.uk>
2014-04-08 16:33:04 +0100

    I made an attempted at the 3 query

--- scripts/SQLqueries.sql
+++ scripts/SQLqueries.sql
@@ -1,13 +1,31 @@
 /*Query 1*/
+
 select staff.workphoneno, staff.homephoneno, staff.name 
 from staff 
 inner join location on staff.locationid = location.locationid 
 inner join building on location.buildingid = building.buildingid
 where building.buildingname = 'Allardyce';
+
 /*Query 2*/
+
 select equipment.name, equipmentallocation.quantity, location.roomno, location.floor 
 from equipmentallocation 
 inner join equipment on equipmentallocation.equipmentid = equipment.equipmentid 
 inner join location on equipmentallocation.locationid = location.locationid 
 inner join building on location.buildingid = building.buildingid 
 where building.buildingname = 'Devon'; 
+
+/*Query 3 needs workin out*/
+
+Select location.type, location.roomno, location.floor, building.buildingname
+from location 
+inner join building on location.buildingid = building.buildingid 
+inner join configuration on location.locationid = configuration.locationid
+where building.buildingname = 'Devon' and configuration.rowcapacity >= 10 
+and location.locationid = 
+(select booking.locationid 
+from booking where booking.bookingdate = '04/11/2013' 
+and booking.endtime = '12:00'  ) 
+group by location.type, location.roomno, location.floor, building.buildingname;
+
+select booking.locationid from booking where booking.bookingdate = '04/11/2013' and booking.endtime = '12:00';
