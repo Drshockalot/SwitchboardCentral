4b1d520f8421845b665198b289eed172680b8971
Lukeeward <b2029843@my.shu.ac.uk>
2014-04-24 18:25:30 +0100

    all tha querys

--- scripts/query4.php
+++ scripts/query4.php
@@ -2,23 +2,22 @@
 
 // Connect to the Oracle database
 require('connection.php'); 
-$organiser = "Dave Whitburn";
+$eventorganiser = "Dave Whitburn";
 $eventname = "queryevent";
 $eventpurpose = "queryeventpurpose";
 // Specify SQL STATEMENT CONTAINING A BIND VARIABLE  
 
 $query1 = "INSERT INTO EVENT (EVENTNAME, EVENTPURPOSE, STAFFID)
-VALUES (:User_Input_EventName, :User_Input_EventPurpose, (SELECT STAFFID 
-                                          FROM STAFF 
-                                          WHERE STAFF.NAME = ':User_Input_Organiser'))";
+VALUES (:User_Input_EventName, :User_Input_EventPurpose, (SELECT STAFFID FROM STAFF WHERE STAFF.NAME = :User_Input_Organiser))";
 
 // Parse the query containing a bind variable.
 $stmt = oci_parse($conn, $query1);
  
 // Bind the value into the parsed statement.
+oci_bind_by_name($stmt, ':User_Input_Organiser', $eventorganiser);
 oci_bind_by_name($stmt, ':User_Input_EventName', $eventname);
 oci_bind_by_name($stmt, ':User_Input_EventPurpose', $eventpurpose);
-oci_bind_by_name($stmt, ':User_Input_Organiser', $organiser);
+
 
 
  
